d91c1f67fc535e06fe363557ec8746f4735c2fbe
Katie Kruzan <31875578+katiekruzan@users.noreply.github.com>
2022-01-26 16:37:07 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+"""
+This is a quick app to pick a student at random. Will add more bells and whistles when the time comes.
+The plan for V1 is a simple commandline program that will return a random student.
+"""
+
