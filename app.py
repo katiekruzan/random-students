234185e1caebac93124468a2e0cb132fce924bfe
Katie Kruzan <31875578+katiekruzan@users.noreply.github.com>
2022-01-26 16:46:29 -0600

    Adding some documentation and initializing the input folder

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ This is a quick app to pick a student at random. Will add more bells and whistle
 The plan for V1 is a simple commandline program that will return a random student.
 """
 
+__author__ = 'Katie Kruzan'
+__version__ = '1.0'
+
+
+def run():
+    return
+
+
+if __name__ == '__main__':
+    # We will expect the students will be in a txt document with one student per line.
+    # This file will be in the \input folder
+
+    run()
